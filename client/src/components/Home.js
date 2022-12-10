3f2607ced9efc9780a31b1d374e1efdc8a4ff20b
Anakha03 <116090176+Anakha03@users.noreply.github.com>
2022-12-10 16:02:41 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,11 +4,28 @@ import './Home.css'
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import CreateIcon from '@mui/icons-material/Create';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 
 
 const Home=()=>{ 
   const navigate=useNavigate()
+  const [value, SetValue] = useState([]);
+
+  useEffect(() => {
+    const fetchAllStudents = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/list");
+        SetValue(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllStudents();
+  }, []);
+
+  console.log(value);
 	
 	return(
 
@@ -32,22 +49,28 @@ const Home=()=>{
       <th scope="col">Phonenumber</th>
     </tr>
   </thead>
+ 
   <tbody>
+   {value.map((student) =>
     <tr >
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>29-01-03</td>
-      <td>mark@gmail.com</td>
-      <td>201</td>
-      <td>993124678</td>
+      <td>{student.rollno}</td>
+      <td>{student.sname}</td>
+      <td>{student.dob}</td>
+      <td>{student.email}</td>
+      <td>{student.cid}</td>
+      <td>{student.pno}</td>
+                               
+      
       <td className='d-flex justify-content-between' >
         <button className='btn btn-success' onClick={()=>{navigate('/view');}}><RemoveRedEyeIcon /></button>
         <button className='btn btn-primary' onClick={()=>{navigate('/edit');}}><CreateIcon /></button>
         <button className='btn btn-danger'><DeleteOutlineIcon /></button>
       </td>
-    </tr>
-
-    
+      
+  
+      </tr>
+  
+  ) }
   </tbody>
 </table>
           </div>
