f588c29237e14e7d0fe89718e8bcda7f0520ada6
Anakha03 <116090176+Anakha03@users.noreply.github.com>
2022-12-10 16:04:36 +0530

    Update view.js

--- client/src/components/view.js
+++ client/src/components/view.js
@@ -3,44 +3,85 @@ import CreateIcon from '@mui/icons-material/Create';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
 
 
 
 const View = () => {
+    const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    const fetchAllStudents = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/list");
+        setValue(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllStudents();
+  }, []);
+
+  console.log(value);
+
+  
+  
+  
+
+
+ /* const handleDelete = async (rollno) => {
+    try {
+      await axios.delete(`http://localhost:8000/delete/:rollno`);
+      window.location.reload()
+    } catch (err) {
+      console.log(err);
+    }
+  };*/
+
+
   return (
     
-       <div className="container mt-3">
-            <h1 style={{ fontWeight: 400 }}>Welcome USER</h1>
-            <Card sx={{ maxWidth: 600 }}>
-                <CardContent>
-                    
-                <div className="add_btn">
-                        <button className="btn btn-primary mx-2"><CreateIcon /></button>
-                        <button className="btn btn-danger"><DeleteOutlineIcon /></button>
+    <div className="container mtp">
+        <h1 style={{ fontWeight: 400 }}>Welcome</h1>
+        <Card sx={{ maxWidth: 600 }}>
+            <CardContent>
+                
+            
+
+                <div className="row">
+                    <div className="left_view col-lg-6 col-md-6 col-12">
+                        <div className="student">
+                            {value.map((student) => (
+                              <div key={student.rollno} className="student">
+                                ------------------------------------------
+                                <p>Student_id: {student.rollno}</p>
+                                <p>Student_name: {student.sname}</p>
+                                <p>DOB: {student.dob}</p>
+                                <p>Email: {student.email}</p>
+                                <p>Course_id: {student.cid}</p>
+                                <p>Phonenumber: {student.pno}</p>
+                               
+                                <button className="btn btn-primary mx-2"><CreateIcon /></button>
+                                <button className="btn btn-danger" /*onClick={handleDelete(student.rollno)}*/><DeleteOutlineIcon /></button>
+                                
+                                </div>
+                              ))}
+                            </div>
                     </div>
-
-                    <div className="row">
-                        <div className="left_view col-lg-6 col-md-6 col-12">
-
-                            <p className="mt-3">Id: <span >1</span></p>
-                            <p className="mt-3"> Name:<span >gjsg</span></p>
-                            <p className="mt-3">Email: <span>gjsag@mail</span></p>
-                            <p className="mt-3">DOB: <span>29092001</span></p>
-                        </div>
-                        <div className="right_view  col-lg-6 col-md-6 col-12">
-
-                            <p className="mt-5">mobile: <span>+913479539</span></p>
-                            <p className="mt-3">Courseid: <span>CSE201</span></p>
-                            <p className="mt-3">Attendance: <span>91</span></p>
+                  
                     
-                        </div>
-                    </div>
+                </div>
 
-                </CardContent>
-            </Card>
+              
 
-    </div>
-  )
+            </CardContent>
+        </Card>
+
+</div>
+)
 }
 
 export default View
